557759ee5efadc29b3c81890e3da9f186b669d01
shunak <shuna966@gmail.com>
2021-08-19 11:41:54 +0900

    wip

--- upload_to_container_registry.sh
+++ upload_to_container_registry.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+if [ $# != 1 ]; then
+    echo "Error: Need your container's image name, input image name as a first argument." $*
+    exit 1
+else
+    echo "Start uploading a new imange to your current GCP project..."
+fi
+
 # Upload to container registry
 gcloud builds submit --tag gcr.io/$(gcloud config get-value project)/$1
 
